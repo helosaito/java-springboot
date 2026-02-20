be07ed8e63b25f7ddc9c50996d6da2c6b2781847
unknown <helo.saito@outlook.com>
2026-02-19 21:50:39 -0300

    classe model criada

---
+++ src/main/java/com/example/saito/models/PessoaModel.java
@@ -0,0 +1,33 @@
+package com.example.saito.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TBL_PESSOA")
+public class PessoaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+
+    public PessoaModel(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
